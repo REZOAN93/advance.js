13ec1c5dc3b3de9ed4d1371f44e41262cbfa4ba9
Nazmul Sarwar <rezoanshawonweb@gmail.com>
2022-01-25 10:52:30 +0600

    truevsfalse

---
+++ true-false.js
@@ -0,0 +1,31 @@
+//Falsy
+//false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+//Truthy
+//'0' any word
+//' '
+ 
+
+const age=0
+
+if (age) {
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+// const name="0"
+let name = null;
+console.log(name);
+if (name) {
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
